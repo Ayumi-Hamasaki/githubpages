c7c3143e057002ef6866787b5048773ac9759f10
Ayumi-Hamasaki <cdybww@live.com>
2020-10-14 16:57:50 +0800

    关于滚动组件的改写

--- src/App.js
+++ src/App.js
@@ -1,26 +1,148 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { Table } from 'antd';
+import 'antd/dist/antd.css';
 import './App.css';
+import { Scrollbars } from 'react-custom-scrollbars';
+const dataSource = [
+    {
+        key: '1',
+        name: '胡彦斌',
+        age: 32,
+        address: '西湖区湖底公园1号',
+    },
+    {
+        key: '42',
+        name: '胡彦祖',
+        age: 42,
+        address: '西湖区湖底公园1号',
+    },
+    {
+        key: '41',
+        name: '胡彦斌',
+        age: 32,
+        address: '西湖区湖底公园1号',
+    },
+    {
+        key: '32',
+        name: '胡彦祖',
+        age: 42,
+        address: '西湖区湖底公园1号',
+    },
+    {
+        key: '31',
+        name: '胡彦斌',
+        age: 32,
+        address: '西湖区湖底公园1号',
+    },
+    {
+        key: '27',
+        name: '胡彦祖',
+        age: 42,
+        address: '西湖区湖底公园1号',
+    },
+    {
+        key: '18',
+        name: '胡彦斌',
+        age: 32,
+        address: '西湖区湖底公园1号',
+    },
+    {
+        key: '21',
+        name: '胡彦祖',
+        age: 42,
+        address: '西湖区湖底公园1号',
+    },
+    {
+        key: '15',
+        name: '胡彦斌',
+        age: 32,
+        address: '西湖区湖底公园1号',
+    },
+    {
+        key: '14',
+        name: '胡彦祖',
+        age: 42,
+        address: '西湖区湖底公园1号',
+    },
+    {
+        key: '13',
+        name: '胡彦斌',
+        age: 32,
+        address: '西湖区湖底公园1号',
+    },
+    {
+        key: '12',
+        name: '胡彦祖',
+        age: 42,
+        address: '西湖区湖底公园1号',
+    },
+    {
+        key: '11',
+        name: '胡彦斌',
+        age: 32,
+        address: '西湖区湖底公园1号',
+    },
+    {
+        key: '9999',
+        name: '胡彦祖',
+        age: 18,
+        address: '西湖区湖底公园1号',
+    },
+];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const columns = [
+    {
+        title: '姓名',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: '年龄',
+        dataIndex: 'age',
+        key: 'age',
+    },
+    {
+        title: '住址',
+        dataIndex: 'address',
+        key: 'address',
+    },
+];
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            scroll: "my_scroll_",
+        };
+    }
+
+    componentDidMount() {
+
+    }
+
+    handleMouseEnter = () => {
+        console.log(`鼠标移入`);
+        this.setState({
+            scroll: 'my_scroll',
+        });
+    }
+
+    handleMouseLeave = () => {
+        console.log(`鼠标移出`);
+        this.setState({
+            scroll: 'my_scroll_',
+        });
+    }
+
+    render() {
+        return (
+            <Scrollbars className={this.state.scroll}
+                        onMouseEnter={() => this.handleMouseEnter()}
+                        onMouseLeave={() => this.handleMouseLeave()}
+                        style={{ width: 500, height: 300 }}>
+                <Table dataSource={dataSource} columns={columns} pagination={false} />
+            </Scrollbars>
+        )
+    };
 }
 
 export default App;
